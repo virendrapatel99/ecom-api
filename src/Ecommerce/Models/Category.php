80640d89c6788eb356244dd602bb4521d7ad02bf
virendrapatel <virendrapatel99@gmail.com>
2018-05-17 22:47:01 +0530

    test commit

--- src/Ecommerce/Models/Category.php
+++ src/Ecommerce/Models/Category.php
@@ -6,10 +6,10 @@ class Category extends \Illuminate\Database\Eloquent\Model {
 
     protected $table = 'category';
     
+    public $frndName =  'Ashu';
+
     public function products(){
         return $this->hasMany('Ecommerce\Models\Products','cat_id');
     }
     
-
-
 }   
